03f4910084ae08e57b4970ecd30f1aac21171b31
Henning <leutz@pcsg.de>
2026-08-20 20:49:58 +0200

    test: allow any configured CI database

--- tests/phpunit-bootstrap.php
+++ tests/phpunit-bootstrap.php
@@ -10,26 +10,3 @@ if (!defined('QUIQQER_AJAX')) {
 
 require_once __DIR__ . '/QUITests/QUI/Countries/DatabaseEnvironment.php';
 require_once __DIR__ . '/../../../../bootstrap.php';
-
-if (QUITests\QUI\Countries\DatabaseEnvironment::usesCiDatabase()) {
-    $databasePlatform = QUI::getDataBaseConnection()->getDatabasePlatform();
-    $databasePlatformClass = $databasePlatform::class;
-    $databaseVendor = QUITests\QUI\Countries\DatabaseEnvironment::getCiVendor();
-
-    if (!$databasePlatform instanceof Doctrine\DBAL\Platforms\AbstractMySQLPlatform) {
-        throw new RuntimeException(
-            'GitLab countries tests expected a MySQL-compatible DBAL platform, got ' . $databasePlatformClass . '.'
-        );
-    }
-
-    $isMariaDbPlatform = str_contains(strtolower($databasePlatformClass), 'maria');
-
-    if (
-        ($databaseVendor === 'mariadb' && !$isMariaDbPlatform)
-        || ($databaseVendor === 'mysql' && $isMariaDbPlatform)
-    ) {
-        throw new RuntimeException(
-            'GitLab DB_VENDOR=' . $databaseVendor . ' does not match DBAL platform ' . $databasePlatformClass . '.'
-        );
-    }
-}
